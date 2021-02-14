15757cf3d2f6db014a15e34d4283aca0a64e371d
Tiberius <mercytiberius@yahoo.com>
2021-02-14 13:24:11 +0300

    randomize object palcement on the board

--- js/app.js
+++ js/app.js
@@ -1,70 +1,85 @@
 let all_cell = document.querySelectorAll("li");
-// let weapon = {
-//   id: 1,
-//   weapon_name: bee_sting,
 
-// }
+const board_cell_array = Array.from(all_cell);
 
 const dimBox = () => {
   for (let i = 0; i < 11; i++) {
-    let random_num = Math.floor(Math.random() * 72);
-    all_cell[random_num].classList = "bg bg-primary";
+    let random_ind = Math.floor(Math.random() * 72);
+    if (
+      board_cell_array[random_ind].textContent == "" &&
+      !all_cell[random_ind].classList.contains("bg-primary")
+    ) {
+      all_cell[random_ind].classList = "bg bg-primary";
+    }
   }
 };
 
 const placeWeapon = () => {
   for (let i = 0; i < 4; i++) {
-    let random_num = Math.floor(Math.random() * 72);
-    if (all_cell[random_num].classList != "bg bg-primary") {
-      all_cell[random_num].textContent = "weapon";
-    }
-  }
-};
-
-const placePlayer = () => {
-  for (let i = 0; i < 3; i++) {
-    let random_num = Math.floor(Math.random() * 72);
+    let random_ind = Math.floor(Math.random() * 72);
     if (
-      all_cell[random_num].classList != "bg bg-primary" &&
-      all_cell[random_num].textContent == ""
+      !all_cell[random_ind].classList.contains("bg-primary") &&
+      board_cell_array[random_ind].textContent == ""
     ) {
-      if (removeExtraPlayer()) {
-        return 0;
-      } else {
-        all_cell[random_num].textContent = "player";
-      }
+      all_cell[random_ind].textContent = "weapon";
     }
   }
 };
 
-const removeExtraPlayer = () => {
-  let count = 0;
-  console.log("yes");
-  all_cell.forEach((cell) => {
-    if (cell.textContent == "player") {
-      console.log(count);
-      if (count === 2) {
-        return true;
-      } else {
-        count += 1;
+window.onload = () => {
+  class Player {
+    constructor(name, score, live, board_position, current_weapon) {
+      (this.name = name),
+        (this.score = score),
+        (this.live = live),
+        (this.board_position = board_position),
+        (this.current_weapon = current_weapon);
+    }
+
+    attack() {
+      alert("I am attacking");
+    }
+    defend() {
+      alert("I am defending");
+    }
+  }
+
+  class Player_1 extends Player {
+    constructor() {
+      super();
+    }
+    position() {
+      let random_ind = Math.floor(Math.random() * 72);
+      if (
+        board_cell_array[random_ind].textContent == "" &&
+        !all_cell[random_ind].classList.contains("bg-primary")
+      ) {
+        board_cell_array[random_ind].textContent = "Player 1";
       }
     }
-  });
-};
-
-class Player {
-  constructor(name, score, live, board_position, current_weapon) {
-    (this.name = name),
-      (this.score = score),
-      (this.live = live),
-      (this.board_position = board_position),
-      (this.current_weapon = current_weapon);
   }
-}
 
-const Player_1 = new Player();
-const Player_2 = new Player();
+  class Player_2 extends Player {
+    constructor() {
+      super();
+    }
+    position() {
+      let random_ind = Math.floor(Math.random() * 72);
+      if (
+        board_cell_array[random_ind].textContent == "" &&
+        !all_cell[random_ind].classList.contains("bg-primary")
+      ) {
+        board_cell_array[random_ind].textContent = "Player 2";
+      }
+    }
+  }
 
-dimBox();
-placeWeapon();
-placePlayer();
+  const player_1 = new Player_1();
+  const player_2 = new Player_2();
+
+  player_1.position();
+  player_2.position();
+
+  dimBox();
+  placeWeapon();
+};
